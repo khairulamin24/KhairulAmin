6c9da7bcb5b406e6b9324743f94b2a48783b7fa6
khairulamin24 <vievzy.acount@gmail.com>
2020-10-31 19:44:25 +0700

    My Biodata With PHP nativ

---
+++ home.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BIODATA</title>
+	<link rel="stylesheet" type="text/css" media="all" href="desain.css">
+</head>
+<body>
+	<div id="atas">
+		<marquee behavior="alternate" direction="rel" scrollamount="8" onmouseover="this.stop(20);" onmouseout="this.start();" >
+		<h2><center>B i o d a t a</center></h2>
+		</marquee>
+	</div>
+
+	<div class="tengah">
+		<div id="isi">
+			<div id="a"><b>
+				<div class="a1">
+					<br/><br/>
+					. Riwayat Pendidikan<br/><br/>
+				</div>
+				<div class="a2">- SD
+				</div>
+				<div class="a3">
+					* SDN 039/007 Pulau Burung<br/><br/>
+				</div>
+				<div class="a2">- SLTP
+				</div>
+				<div class="a3">
+					* YPI MTs Nurul Mubtadi'in<br/><br/>
+				</div>
+				<div class="a2">- SLTA
+				</div>
+				<div class="a3">
+					* YPI MA Nurul Mubtadi'in<br/><br/>
+				</div>
+				<div class="a2">- Perguruan Tinggi
+				</div>
+				<div class="a3">
+					* Universitas Abdurrab<br/><br/><br/><br/>
+				</div>
+					</b>
+			</div>
+			<div id="b">
+					
+			</div>
+			<div id="c">
+					<table class="isi" cellspacing="0" cellpadding="5">
+						<tr>
+							<tr>
+								<td id="baris">Nama</td>
+								<td>:</td>
+								<td>Khairul Amin</td>
+							</tr>
+							<tr>
+								<td>Jenis Kelamin</td>
+								<td>:</td>
+								<td>Laki-Laki</td>
+							</tr>
+							<tr>
+								<td>NIK</td>
+								<td>:</td>
+								<td>1404172405960002</td>
+							</tr>
+							<tr>
+								<td>Tempat Lahir</td>
+								<td>:</td>
+								<td>Kuala Gaung</td>
+							</tr>
+							<tr>
+								<td rowspan="2">Tanggal Lahir</td>
+								<td rowspan="2">:</td>
+								<td>24 Mei 1996M</td>
+								
+							</tr>
+							<tr>
+								<td>17 Syawal 1417H</td>
+							</tr>
+							<tr>
+								<td>Agama</td>
+								<td>:</td>
+								<td>Islam</td>
+							</tr>
+							<tr>
+								<td>Kewarganegaraan</td>
+								<td>:</td>
+								<td>Indonesia</td>
+							</tr>
+							<tr>
+								<td>Alamat</td>
+								<td>:</td>
+								<td>Jln. Tri Marga Prum. Taman Karya Asri Blo H No. 06</td>
+							</tr>
+							<tr>
+								<td>Pekerjaan</td>
+								<td>:</td>
+								<td>Mahasiswa</td>
+							</tr>
+							<tr>
+								<td>Status</td>
+								<td>:</td>
+								<td>Belum Menikah</td>
+							</tr>
+							<tr>
+								<td>Hobi</td>
+								<td>:</td>
+								<td>Melakukan Hal Yg Memicu Adrenali</td>
+							</tr>
+							<tr>
+								<td>No Hp</td>
+								<td rowspan="2">:</td>
+								<td rowspan="2">+6281372890230</td>
+							</tr>
+							<tr>
+								<td>No WA</td>
+							</tr>
+							<tr>
+								<td rowspan="3">Email</td>
+								<td rowspan="3">:</td>
+								<td>khairul.amin@student.univrab.ac.id</td>
+							</tr>
+							<tr>
+								<td>khairulamin.ari24@gmail.com</td>
+							</tr>
+							<tr>
+								<td>aminkhairul783@gmail.com</td>
+							</tr>
+							<tr>
+								<td>Facebook</td>
+								<td>:</td>
+								<td><a href="https://web.facebook.com/login.php?login_attempt=1&lwv=111">arie banjar pulauburung</a></td>
+							</tr>
+							<tr>
+								<td>Instagram</td>
+								<td>:</td>
+								<td><a href="https://www.instagram.com/accounts/login/?hl=id">khairul_amin_ari24</a></td>
+							</tr>
+						</tr>
+						
+					</table>
+			</div>
+			<div id="d"><b>
+					<div class="d1"><br/><br/>
+						. Pengalaman<br/><br/>
+					</div>
+					<div class="d2">
+						- PT. RSUP-IND<br/><br/>
+					</div>
+					<div class="d3">
+						> WHK (4 Bulan) <<br/>
+						* 1 Bulan (TCA)<br/>
+						* 3 Bulan (DC)<br/><br/>
+					</div>
+					<div class="d2">
+						- PT. RSUP-PKB<br/><br/>
+					</div>
+					<div class="d3">
+						>SECURITY (21 Bulan) <<br/>
+						* 2 Bulan (Wilayah 2) Kecamatan Kateman<br/>
+						* 4 Bulan (Wilayah 3)<br/>
+						* 15 Bulan (KM 6.5)<br/><br/><br/>
+					</div>
+				</b>
+			</div>
+		</div>
+	</div>
+
+	<div class="bawah">
+		
+	</div>
+</body>
+</html>
